7ebc23e56326c108f227fc1bd5f59388476696e4
Engrat <71091892+PolcioHubi@users.noreply.github.com>
2025-12-15 19:50:08 +0100

    zmiana url

--- deploy_ubuntu.sh
+++ deploy_ubuntu.sh
@@ -13,7 +13,7 @@ set -e
 SERVICE_NAME="mobywatel"
 PROJECT_USER="mobywatel_user"
 DEST_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DOMAIN="185-167-99-62.cloud-xip.com"
+DOMAIN="212-115-110-157.cloud-xip.com"
 SSL_EMAIL="polciovps@atomicmail.io"
 GUNICORN_WORKERS=$((2 * $(nproc) + 1))
 # POPRAWKA: Polityka CSP dopasowana do potrzeb aplikacji (QR codes, fonty zewnętrzne, Service Worker)
